b432090868087f04043de309ff67b21656aba655
isaMuneretto <isamuneretto@gmail.com>
2024-03-18 21:47:08 -0300

    exercise estudante

---
+++ src/Estudante/Estudante.java
@@ -0,0 +1,13 @@
+package Estudante;
+
+public class Estudante {
+    String nome;
+    double notas[];
+
+    public void adicionarNota(){
+        notas++;
+    }
+    public double calcularMedia(double notas){
+
+    }
+}
